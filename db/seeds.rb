3f24ba76dceca6cda0c9107f0baf616c1769cd74
Sagar Shah <sgrshah@gmail.com>
2013-09-24 19:22:02 -0400

    Parser can scrape wiki for reps names and districts. Duplicate code in seed file; decision of where to keep needs to be made. Potentially could be refactored to be more object oriented and extensible

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,14 @@
-Congressman.create(:first_name => "Robert", :last_name => "Menendez")
+require 'open-uri'
+
+webpage = open('http://en.wikipedia.org/wiki/List_of_current_members_of_the_United_States_House_of_Representatives_by_seniority')
+doc = Nokogiri::HTML(webpage)
+names_array = doc.search('td:nth-child(2)').collect{|hyperlink| hyperlink.text}.select{|item| item.match('\A\w')}
+districts_array = doc.search('td:nth-child(4)').collect{|hyperlink| hyperlink.text}.select{|item| item.match('\A\w')}
+
+
+
+names_array.each do |congressman|
+	districts_array.each do |district|
+		Congressman.create(:name => congressman, :district => district)
+	end
+end
